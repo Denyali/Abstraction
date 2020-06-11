21340f02c9a530b753749c5b4a4ec9fc1424ca28
Deny Ali Saputra <35654955+Denyali@users.noreply.github.com>
2020-06-11 14:56:55 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Abstraction.AbstractClass; //Abstract Class//
-//Abstraction.Interface  //Interface
+//Abstraction.Interface  //Interface//
 
 namespace Abstraction
 {
